6556c099e9f2faa15ab202f6f53974cbb0789d04
Cuphat <matthew@cuphat.com>
2018-12-18 22:30:35 -0500

    Add project files.

---
+++ OoT_Decompressor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OoT_Decompressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
